0c7fc1a62833cc945d6281c2c515c8e00d714751
raghu prasad <prasadraghuks@gmail.com>
2020-02-10 06:40:12 +0530

    added end to end

---
+++ endtoend/Angular8-SpringBoot-CRUD-Tutorial/angular8-springboot-client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Full Stack End to End using Angular 8 and Java Spring Boot';
+}
